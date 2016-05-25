f3ee9945a7bc9009cdfabc6c5730e74e5afd6fa6
Brad Smith <brad.alan.smith@gmail.com>
2016-05-25 09:21:02 -0700

    spa nohash

--- geophoto/static/js/article.js
+++ geophoto/static/js/article.js
@@ -3,7 +3,7 @@ var ArticleView = BaseView.extend({
     el: '#main',
     template: $("#article_templ").html(),
     initialize: function (key) {
-        $.getJSON('/articles/' + key, _.bind(
+        $.getJSON('/articles/' + key + "/?format=json", _.bind(
         	function (d) {
 	        	this.data = d;
 	        	BaseView.prototype.initialize.call(this);
--- geophoto/views.py
+++ geophoto/views.py
@@ -1,5 +1,6 @@
 
 import os
+from functools import wraps
 
 from flask import Flask, jsonify, render_template, request, abort
 
@@ -8,23 +9,36 @@ from geophoto import app, models, oembed
 basedir = os.path.dirname(os.path.abspath(__file__))
 index_path = os.path.join(basedir,'static','index.html')
 
+def spa(vfunc):
+	@wraps(vfunc)
+	def wrapper(*args, **kwargs):
+		if request.args.get('format') == 'json':
+			return vfunc(*args, **kwargs)
+		else:
+			return render_template('index.html')
+	return wrapper
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/photos/")
+@spa
 def photos():
     return jsonify(models.photos())
 
 @app.route("/articles/")
+@spa
 def articles():
     return jsonify(models.articles())
 
 @app.route("/articles/<key>/")
+@spa
 def article(key):
 	return jsonify(models.article(key))
 
 @app.route("/tags/<id>/", methods=["GET",]) 
+@spa
 def tags(id):
     return jsonify(models.tags(id))
 
@@ -33,6 +47,7 @@ def oembed_service():
     return jsonify(oembed.oembed(request.args.get('url')))
 
 @app.route("/tags/<id>/", methods=["POST",]) 
+@spa
 def add_tags(id):
     try:
         return jsonify(models.add_tags(id, request.get_json()))
